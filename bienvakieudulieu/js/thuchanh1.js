435e9142d7a5faaded7ac435b43389ef6c1dad4d
vuongvantu <vuongvantu2001x@gmail.com>
2021-04-06 15:15:48 +0700

    bienvakieudulieu

---
+++ bienvakieudulieu/js/thuchanh1.js
@@ -0,0 +1,39 @@
+
+//bài 1
+let i=10;
+let f=20.5;
+let c=true;
+let s="Hà Nội";
+
+document.write('i='+i)
+document.write('<br/>')
+document.write('f='+f)
+document.write('<br/>')
+document.write('b='+c)
+document.write('<Br/>')
+document.write('s='+s)
+document.write('<br/>')
+
+//bài 2
+let width = 20;
+let height = 10;
+let area = width * height;
+document.write('area='+area);
+//bài 3
+let a;
+let b;
+a=prompt('a=');
+b=prompt('b=');
+if(a%b===0){
+    alert('Có');
+}
+else{
+    alert('Không');
+}
+
+
+
+
+
+
+
